3a983720336ebf523ec3329219b32044b38c7fcc
Yang-JiaLiang <1461027098@qq.com>
2021-09-05 16:55:32 +0800

    2

---
+++ idea_test/src/com/itheima/helloworld.java
@@ -0,0 +1,9 @@
+package com.itheima;
+
+public class helloworld {
+    public static void main(String[] arr){
+        System.out.print(" HELLO WORLD");
+    }
+
+}
+
---
+++ idea_test/src/com/itheima/一生能过闰年.java
@@ -0,0 +1,25 @@
+package com.itheima;
+
+import java.util.Scanner;
+
+public class 一生能过闰年 {
+    public static void main(String[] args) {
+        System.out.println("请输入出生年份：");
+        Scanner s = new Scanner(System.in);
+        int a = Integer.parseInt(s.next());
+        System.out.println(a);
+
+        int count=0;
+        for (int i=a;i<a+100;i++)
+        {
+            if((i%4==0&&i%100!=0)||(i%400==0))
+             count++;
+        }
+
+        System.out.println(count);
+
+        }
+
+
+}
+
