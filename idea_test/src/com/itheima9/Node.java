a7c3ce687ef408e6e9e8527b9ab7c47221481ba2
Yang-JiaLiang <1461027098@qq.com>
2021-10-26 20:13:37 +0800

    作业

---
+++ idea_test/src/com/itheima9/Node.java
@@ -0,0 +1,22 @@
+package com.itheima9;
+
+public class Node<T> {
+    public T data;
+    public Node<T> next;
+
+    public Node() {
+        this(null);
+    }
+
+    public Node(T data) {
+        this(data, null);
+    }
+
+    public Node(T data, Node<T> next) {
+        this.data = data;
+        this.next = next;
+    }
+
+}
+
+
